5fcf3115be6d120f17875779614177c1937336a2
Daniel <viridianrobotics@gmail.com>
2019-02-18 15:56:58 -0600

    Worked on autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZoDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZoDriving.java
@@ -401,7 +401,7 @@ public class ZoDriving extends LinearOpMode {
         //robot.servoMark.setPosition(.9);
     }
     public void MoveHookUp(boolean direction){
-        double distance = .80;
+        double distance = 2;
         if(direction){
             hookEncoder(3, distance, 10);
         }else{
